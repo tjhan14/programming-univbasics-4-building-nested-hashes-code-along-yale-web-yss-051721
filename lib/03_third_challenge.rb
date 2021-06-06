bcbfe6506936ebef69846e0372150d0abbe8f2c5
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-06 11:37:29 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,31 +1,17 @@
-require pry
-
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague, age: "53"
-        #your key/value pairs here
-        },
-      :matriarch => {name: "Lady Montague, age: "54"
-        #your key/value pairs here
-        },
-      :hero => {name: "Romeo", age: "15", status: "alive"
-        #your key/value pairs here
-        },
+    :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"
-        #your key/value pairs here
-        },
-      :matriarch => {name: "Lady Capulet", age: "51"
-        #your key/value pairs here
-        },
-      :heroine => {name: "Juliet", age: "15", status: "alive"
-        #your key/value pairs here
-        },
+    },
+    :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
-   }
+    }
   }
 end
 
